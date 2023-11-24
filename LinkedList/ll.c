34ff343798266bfea005daa573d4d4306e4bdc97
Cristiaaannn <cristianmirea@icloud.com>
2023-11-24 02:16:35 +0200

    Added insert head / first feature

--- LinkedList/ll.c
+++ LinkedList/ll.c
@@ -4,11 +4,11 @@
  * Brief:   Program that show ll priciples, academic purposes
  */
 
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -25,14 +25,14 @@ int counter = 1;
 /*
  * Initializes the linked list
  */
-Node* initialize() {
+Node *initialize() {
     int nrElAtInit = 0;
 
-    Node* new_node = NULL, *ptr = NULL;
+    Node *new_node = NULL, *ptr = NULL;
 
     printf("\nWith how many elements do you want to init the list?: ");
     scanf("%d", &nrElAtInit);
-    new_node = (Node*)malloc(sizeof(Node));
+    new_node = (Node *)malloc(sizeof(Node));
     printf("Enter node(%d) data: ", counter++);
     scanf("%d", &new_node->data);
     new_node->next = NULL;
@@ -64,25 +64,25 @@ void displayMenu() {
     printf("How many elements does the list have? (count)\n");
     printf("Sum of the linked list's elements (sum)\n");
     printf("Find an element in the linked list (search)\n");
+    printf("Insert an element before the first (insert first / head)\n");
     printf("Exit (exit)\n");
 }
 /*
  * Displays the linked list on the open stream
  */
-void display(Node* first) {
+void display(Node *first) {
+    FILE *fp;
 
-    FILE* fp;
-
-    fp = fopen("list.txt","w");
+    fp = fopen("list.txt", "w");
     if (fp == NULL) {
         perror("\nerror opening file");
     }
 
     Node *ptr = first;
     for (int i = 0; i < counter - 1; i++) {
-        fprintf(fp,"%d ", ptr->data);
+        fprintf(fp, "%d ", ptr->data);
         if (ptr->next != NULL) {
-            fprintf(fp,"-> ");
+            fprintf(fp, "-> ");
         }
         ptr = ptr->next;
     }
@@ -94,7 +94,7 @@ void display(Node* first) {
 /*
  * Thread function for calling display()
  */
-void* display_thread(void* arg) {
+void *display_thread(void *arg) {
     while (1) {
         pthread_mutex_lock(&mutex);
         display(first);
@@ -107,8 +107,7 @@ void* display_thread(void* arg) {
 /*
  * Returns the sum of the linked list's elements
  */
-int sum(Node* first) {
-
+int sum(Node *first) {
     Node *ptr = first;
     int sum = 0;
 
@@ -122,8 +121,7 @@ int sum(Node* first) {
 /*
  * Returns the max. element of the linked list
  */
-int max(Node* first) {
-
+int max(Node *first) {
     Node *ptr = first;
     int max = 0;
 
@@ -138,7 +136,7 @@ int max(Node* first) {
 
 /*
  * Returns 1 if the key was found in the linked list, 0 otherwise
- * Found keys are transposed with the previous key (transposition optimization) 
+ * Found keys are transposed with the previous key (transposition optimization)
  */
 int search(Node *first, int key) {
     Node *swap = NULL, *ptr = first, *q = first;
@@ -166,10 +164,23 @@ int search(Node *first, int key) {
     return found;
 }
 
-int main(int argc, char **argv) {
-    int option, key;
-    char input[20];
+/*
+ * Inserts a new node before the first node
+ */
+Node *insertBeforeFirst(Node *first, int x) {
+    Node *t = NULL;
+    t = (Node *)malloc(sizeof(Node));
+    t->data = x;
+    t->next = first;
+    first = t;
+    printf("🚀 %d was inserted before the first node\n", x);
+    counter++;
+    return first;
+}
 
+int main(int argc, char **argv) {
+    int key, i = 0;
+    char input[50];
 
     if (first == NULL) {
         printf("\nLinked list is empty, initializing \n");
@@ -185,11 +196,18 @@ int main(int argc, char **argv) {
     displayMenu();
 
     do {
-        if (strcmp(input,"menu") == 0) {
-        displayMenu();
+        if (strcmp(input, "menu") == 0) {
+            displayMenu();
         }
         printf("Enter cmd: ");
-        scanf("%s", input);
+        fgets(input, 50, stdin);          // read the entire line
+        input[strcspn(input, "\n")] = 0;  // remove the newline character
+
+        if (strncmp(input, "insert head", 11) == 0 || strncmp(input, "insert first", 12) == 0) {
+            sscanf(input, "insert head %d",
+                   &key);  // parse the argument from the input string
+            first = insertBeforeFirst(first, key);
+        }
         if (strcmp(input, "reinitialize") == 0) {
             pthread_mutex_lock(&mutex);
             counter = 1;
@@ -201,21 +219,20 @@ int main(int argc, char **argv) {
             printf("The list has %d elements\n", counter - 1);
         }
         if (strcmp(input, "sum") == 0) {
-            printf("The \u03A3 of the %d elements is %d\n", counter - 1, sum(first));
+            printf("The \u03A3 of the %d elements is %d\n", counter - 1,
+                   sum(first));
         }
         if (strcmp(input, "max") == 0) {
-            printf("The max. of the %d elements is %d \U0001F60E\n", counter - 1, max(first));
+            printf("The max. of the %d elements is %d \U0001F60E\n",
+                   counter - 1, max(first));
         }
-        if (strcmp(input, "search") == 0 || strcmp(input, "find") == 0) {
-            scanf("%d", &key);
+        if (strncmp(input, "search", 6) == 0 || strncmp(input, "find", 4) == 0) {
+            sscanf(input, "%*s %d", &key);  // parse the argument from the input string
             if (search(first, key)) {
-                printf("%d was found \033[0;32m\u2713\033[0m\n", key); // Green checkmark for found
+                printf("%d was found \033[0;32m\u2713\033[0m\n", key);  // Green checkmark for found
             } else {
-                printf("%d was not found \033[0;31m\u2717\033[0m\n", key); // Red cross for not found
+                printf("%d was not found \033[0;31m\u2717\033[0m\n", key);  // Red X for not found
             }
-        }
-        if (strcmp(input, "add") == 0) {
-            printf("Adding\n");
         }
         if (strcmp(input, "remove") == 0) {
             printf("Removing\n");
